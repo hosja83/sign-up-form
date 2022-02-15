fc4f424b13136b816226fc5e251f5e5a918130f2
Alhosainy Altaher <alhosainy.altaher@gmail.com>
2022-02-15 16:59:28 -0500

    Completed Sign-up form template

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+const password = document.getElementById('password');
+const confirmation = document.getElementById('confirm-password');
+const form = document.getElementById('create-account');
+const label = document.querySelector('.password-label');
+
+form.addEventListener('submit', handleMismatchPasswords);
+
+function handleMismatchPasswords(event) {
+  event.preventDefault();
+  const exists = document.querySelector('small') || false;
+  if (exists) label.removeChild(document.querySelector('small'));
+  if (password.value !== confirmation.value) {
+    let mismatch = document.createElement('small');
+    label.appendChild(mismatch);
+    mismatch.textContent = "*Passwords do not match";
+    mismatch.style.color = "#c13838";
+    confirmation.value = "";
+    confirmation.style = "border: 2px solid #c13838;";
+    password.style = "border: 2px solid #c13838;";
+    confirmation.addEventListener('focus', removeMismatchEvents);
+    password.addEventListener('focus', removeMismatchEvents);
+  }
+}
+
+function removeMismatchEvents() {
+  let mismatch = document.querySelector('small');
+  password.style.border = "none";
+  confirmation.style.border = "none";
+  password.removeEventListener('focus', removeMismatchEvents);
+  confirmation.removeEventListener('focus', removeMismatchEvents);
+  label.removeChild(mismatch);
+}
